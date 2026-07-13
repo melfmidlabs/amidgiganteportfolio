9ead12d38df1b0f2d56aaaf5a2629b0ff200c63b
Melfmid21 <102124361+Melfmid21@users.noreply.github.com>
2026-07-13 16:03:22 +0800

    adding project details

--- Models/ProjectModel.cs
+++ Models/ProjectModel.cs
@@ -2,15 +2,25 @@
 {
     public class ProjectModel
     {
+        public int Id { get; set; }
+        public string Number { get; set; } = string.Empty;
         public string Title { get; set; } = string.Empty;
+
+        public string Category { get; set; } = string.Empty;
+
+        public string Year { get; set; } = string.Empty;
+
+        public bool Featured { get; set; }
 
         public string Description { get; set; } = string.Empty;
 
         public string ImageUrl { get; set; } = string.Empty;
 
-        public string GitHubUrl { get; set; } = string.Empty;
-        public string Technologies { get; set; } = string.Empty;
+        public List<string> Technologies { get; set; } = [];
 
-        public string LiveDemoUrl { get; set; } = string.Empty;
+        public string LiveUrl { get; set; } = string.Empty;
+        public string GithubUrl { get; set; } = string.Empty;
+
+        public List<string> Images { get; set; } = [];
     }
 }
